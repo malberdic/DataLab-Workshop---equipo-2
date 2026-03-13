368af41a0bb066f34c3ffc7af3bbded0015be539
malberdic <mati.alberdi@gmail.com>
2026-03-13 15:32:34 -0300

    sraper wraper y persistencia

--- src/DataAccess/Storage/csv_review_repository.py
+++ src/DataAccess/Storage/csv_review_repository.py
@@ -1,10 +1,10 @@
-from pandas import DataFrame as pdf
+import pandas as pd
 
 
 class CSVReviewRepository:
 
-    def save(self, reviews: pdf, path: str):
+    def save(self, reviews: pd.DataFrame, path: str):
         reviews.to_csv(path, index=False)
 
-    def load(self, path: str) -> pdf:
-        return pdf.from_csv(path)
+    def load(self, path: str) -> pd.DataFrame:
+        return pd.read_csv(path)
